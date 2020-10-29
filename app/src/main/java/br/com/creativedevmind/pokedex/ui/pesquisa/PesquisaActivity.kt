0e0f882adc9dabef0c43503b8118c12c79959921
felipe_c_rodrigues <felipe.cavalcante.rod@gmail.com>
2020-10-28 22:20:28 -0300

    Exibição da pesquisa do Pokémon

--- app/src/main/java/br/com/creativedevmind/pokedex/ui/pesquisa/PesquisaActivity.kt
+++ app/src/main/java/br/com/creativedevmind/pokedex/ui/pesquisa/PesquisaActivity.kt
@@ -2,16 +2,40 @@ package br.com.creativedevmind.pokedex.ui.pesquisa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.creativedevmind.pokedex.R
+import br.com.creativedevmind.pokedex.data.remote.api.APIService
+import br.com.creativedevmind.pokedex.data.repository.PokemonRepositoryImpl
+import br.com.creativedevmind.pokedex.domain.repository.PokemonRepository
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.btPesquisar
+import kotlinx.android.synthetic.main.activity_pesquisa.*
 
 class PesquisaActivity : AppCompatActivity() {
+
+    private lateinit var pokemonRepository: PokemonRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pesquisa)
 
+        pokemonRepository = PokemonRepositoryImpl(APIService.instance)
+
         btPesquisar.setOnClickListener {
 
+            pokemonRepository.pesquisar(etPokemon.text.toString(),
+                {
+                    tvNomePokemon.text = it?.nome
+                    Picasso.get().load(it?.urlImagem).into(ivPokemon)
+                },
+                {
+                    Toast.makeText(this@PesquisaActivity, "Pokémon não encotrando", Toast.LENGTH_SHORT).show()
+                }
+            )
+
+            //tvNomePokemon.text
+            //
         }
     }
 }
